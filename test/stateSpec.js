d000b790c6672bd7b09ffab0de7e381a255f25b4
Brandon Poe <brandon.poe@union.co>
2016-04-21 21:31:57 -0400

    added unit tests

--- test/stateSpec.js
+++ test/stateSpec.js
@@ -1,4 +1,51 @@
 describe('The state service', function() {
 
-    it('can store a value to each element in an array');
+    beforeEach(function() {
+        this.node = document.createElement('div');
+    });
+
+    it('can check for the presence of a class', function() {
+
+        this.node.className = 'is-active';
+
+        expect(state(this.node, 'active')).toBe(true);
+    });
+
+    it('can add a class', function() {
+
+        state(this.node, 'active', true);
+
+        expect(this.node.className).toBe('is-active');
+    });
+
+    it('can remove a class', function() {
+
+        this.node.className = 'is-active';
+
+        state(this.node, 'active', false);
+
+        expect(this.node.className).toBe('');
+    });
+
+    it('can toggle a class', function() {
+
+        state(this.node, 'active', 'toggle');
+
+        expect(this.node.className).toBe('is-active');
+
+        state(this.node, 'active', 'toggle');
+
+        expect(this.node.className).toBe('');
+    });
+
+    it('works with svg elements', function() {
+
+        var node = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+        state(node, 'active', true);
+
+        expect(node.className.baseVal).toBe('is-active');
+
+        expect(state(node, 'active')).toBe(true);
+    });
 });
